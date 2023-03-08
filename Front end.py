e812edf47c987b034a22778208bfbc90f1c2b1cf
RijeshKrishna <126497087+RijeshKrishna@users.noreply.github.com>
2023-03-08 15:15:33 +0530

    Add files via upload

---
+++ Front end.py
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hospital</title>
+<style>
+html {scroll-behavior: smooth;}
+body{background-color:#2980b9}
+h1{background-color:white; color:red; font-size:54px}
+h3{color:#db1a1a; font-size:24px}
+h2{background-color:#db1a1a; color:white; font-size:32px}
+a{text-decoration: none; color:white; font-size:18px;}
+ol{color:white; font-size:18px}
+.para{background-color:white; color:red;}
+.column{float: left; width: 23%; padding: 0 10px;}
+.flip-card {background-color: transparent; width: 275px; height: 275px;}
+.flip-card-inner {position: relative; width: 100%; height: 100%; text-align: center; transition: transform 0.6s;
+  transform-style: preserve-3d; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);}
+.flip-card:hover .flip-card-inner { transform: rotateY(180deg);}
+.flip-card-front, .flip-card-back { position: absolute; width: 100%; height: 100%; backface-visibility: hidden;}
+.flip-card-front { background-color: blue; color: black;}
+.flip-card-back { background-color: #db1a1a; color: white; transform: rotateY(180deg); font-size:20px;}
+.button1{padding:20px; border-radius:12px; background-color: #db1a1a; border-color:white; color:white; font-size:18px;}
+.button2{padding:20px; background-color: #db1a1a; border-color:#2980b9; text-align:left}
+.sticky {position: -webkit-sticky; position: sticky; top: 0;}
+</style>
+</head>
+<body>
+
+<h1 id="top" bgcolor="white" align="center">Hospital</h1>
+
+<button class="button2"><b style="color:white; font-size:22px;">Direct Links</b><br><br><a href="#ES">Emergency Services</a><br><a href="#PD">Pediatrics</a>
+<br><a href="#CC">Cardiac Cares</a><br><a href="#GS">General Surgery</a><br><a href="#NR">Neurology</a><br>
+<a href="#IM">Internal Medicine</a><br><a href="#PT">Pathology</a><br><a href="#PH">Physiotherapy</a></button>
+<img src="t1.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px align="right" alt="image">
+<img src="t2.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px align="right" alt="image">
+<img src="t3.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px align="right" alt="image">
+<img src="t4.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px align="right" alt="image">
+
+<h2 id="ES">Emergency Services</h2>
+<p class="para">An emergency department (ED), also known as an accident and emergency department (A&E), emergency room (ER), 
+emergency ward (EW) or casualty department, is a medical treatment facility specializing in emergency medicine, 
+the acute care of patients who present without prior appointment; either by their own means or by that of an ambulance. 
+The emergency department is usually found in a hospital or other primary care center.
+Due to the unplanned nature of patient attendance, the department must provide initial treatment for a broad spectrum of 
+illnesses and injuries, some of which may be life-threatening and require immediate attention. In some countries, emergency 
+departments have become important entry points for those without other means of access to medical care.</p>
+<img src="t5.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=300px height=175px align="right" alt="image">
+
+<h3>Critical conditions handled in Emergency Department</h3>
+<ol>
+<li>Cardiac arrest</li>
+<li>Heart attack</li>
+<li>Trauma</li>
+<li>Mental illness</li>
+<li>Asthma and COPD</li>
+<li>Accidnets</li>
+</ol>
+
+<h2 id="PD">Pediatrics</h2>
+<p class="para">Pediatrics is a field of medicine that needs peerless expertise and skills as the doctors have to deal with infants and 
+children. Accurate evaluation, diagnosis and treatment are very important to get the best results. Pediatric & Neonatal 
+Intensive Care Units of Vasundhara Hospital are specialized in the care of premature infants, sick babies.
+We offer Advanced Pediatric & Neonatal Intensive Care services with support of all Pediatric subspecialties under one roof. 
+The availability of Neonatal Intensive Care has improved outcomes for high-risk infants including those born preterm or with 
+serious medical or surgical conditions. Our 15 bedded the State-of-Art Level-III NICU, is well supported by very experienced 
+Pediatric Team and Nurses.</p><br><center><img src="t6.jfif" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+<h2 id="CC">Cardiac Care</h2>
+<p class="para">The Cardiac Care Unit of our hospital aspires to deliver cardiac care in a most comprehensive and affordable way to a 
+common generous. The hospital has well trained doctors, staff members and infrastructure to provide best cardiac care to the 
+patient. In the non-invasive lab there are various packages to screen the cardiac illness and to treat appropriately with 
+the help of trained staff and sophisticated advance equipment’s. Centre is equipped with advanced Non-Invasive Cardiology 
+tests which has Echocardiography machine, TMT, Holter, CCU, Emergency Services & Heart Health Check-ups. Apart from this, 
+consultants participate in various health camps conducted by the hospital near surrounding villages to provide to patients 
+and promote awareness about the cardiac diseases.</p><center><img src="t9.jfif" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+<h2 id="GS">General Surgery</h2>
+<p class="para">General surgery is a surgical specialty that focuses on alimentary canal and abdominal contents including the esophagus, 
+stomach, small intestine, large intestine, liver, pancreas, gallbladder, appendix and bile ducts, and often the thyroid gland.
+ They also deal with diseases involving the skin, breast, soft tissue, trauma, peripheral artery disease and hernias and 
+perform endoscopic procedures such as gastroscopy and colonoscopy.<img src="t10.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px align="right" alt="image">
+<h3>We offer surgery for :-</h3>
+<ol bgcolor="white">
+<li>Laparoscopic surgery</li>
+<li>Vascular surgery</li>
+<li>Endocrine surgery</li>
+<li>Transplant surgery</li>
+<li>Colorectal surgery</li>
+<li>Surgical oncology</li>
+</ol>
+
+<h2 id="NR">Neurology</h2>
+<p class="para">Our team of highly qualified doctors are specialities in many services like Stroke Clinic, Epilepsy, Parkinsonism and 
+Related Disorder, Migraine and all type of headaches, Back and Cervical Pain, Muscle Weakness etc. All the advanced lab 
+investigations studies like NCV (Nerve Conduction Study), EMG (Electro Myo Graphy), RNS (Repetitive Nerve Stimulation), 
+VEP (Visual Evoked Potentials) and EEG (Electro Encephalo Gram) and Imaging studies like advanced MRI scans and CT scans, 
+are available on hospital. A complete Neuro Unit is functional 24 x 7.</p>
+<center><img src="t11.jfif" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+
+<h2 id="IM">Internal Medicine</h2>
+<p class="para">Internal medicine or general internal medicine is the medical specialty dealing with the prevention, diagnosis, and 
+treatment of internal diseases. Doctors specializing in internal medicine are called internists, or physicians. Internists 
+are medical specialists that are skilled in the management of patients who have undifferentiated or multi-system disease 
+processes. Physicians care for hospitalized (inpatient) and ambulatory (outpatient) patients and may play a major role in 
+teaching and research.</p>
+<p class="para">The department of Internal Medicine offers high-quality of diagnostic and treatment options through encompassed efforts 
+of competent doctors. Our physicians are committed to applying latest medical knowledge and technology to meet the needs of 
+our patients of General and Varied Medical Ailments.</p><center><img src="t12.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+
+<h2 id="PT">Pathology</h2>
+<p class="para">Our hospital’s Pathology Service bridges science and clinical medicine to provide full clinical and research based 
+services. Our clinical laboratory provides clinicians with information that enables duly patient diagnosis and treatment.
+Lab is equipped with fully automated state-of-art technology and infrastructure working Round the Clock 24x7. Our 
+standardized processes and protocols, in-hospital presence, clinical interaction and feedback ensure round-the-clock 
+stringent quality monitoring. Pathology Services at our hospital aim to empower accurate insights in the test 
+reports for right diagnosis and treatment. We believe that every test report must be accurate to aid in the correct 
+treatment and early recovery of patients.</p><center><img src="t13.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+
+
+
+<h2 id="PH">Physiotherapy</h2>
+<p class="para">Physiotherapy is also called "physical therapy". It attempts to treat what limits a person's abilities to move and do 
+things in their daily life. It is the treatment of disease, injury, deformity or disability to improve the quality of life. 
+Physiotherapists use exercise, hands-on techniques (including manual therapy, dry needling and trigger point therapy), 
+patient education and various other methods.</p>
+<p class="para">Our team of Physiotherapists works closely with Medical Specialists, helping patients Post surgeries. It is concerned 
+with human function, movement and maximizing Physical Potential within the spheres of growth, prevention, treatment or 
+intervention and habilitation.</p><center><img src="t14.jpg" onmouseover="bigImg(this)" onmouseout="normalImg(this)" width=275px height=175px alt="image"></center>
+
+<h2>Best Doctors in our hospital</h2>
+
+<div class="column">
+<div class="flip-card">
+<div class="flip-card-inner">
+<div class="flip-card-front">
+<img src="avatar5.png" alt="Avatar" style="width:275px;height:275px;" alt="image">
+</div>
+<div class="flip-card-back">
+<h1>Dr. Sharma</h1> 
+<p><b>Surgeon</b></p> 
+<p>Experience - 25 years<br>He has performed more than 150 surgery.</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="column">
+<div class="flip-card">
+<div class="flip-card-inner">
+<div class="flip-card-front">
+<img src="avatar6.jpg" alt="Avatar" style="width:275px;height:275px;" alt="image">
+</div>
+<div class="flip-card-back">
+<h1>Dr. Joshi</h1> 
+<p><b>General Physician</b></p> 
+<p>Experience - 35 years<br>He has received Padma Bhushan in 2017.</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="column">
+<div class="flip-card">
+<div class="flip-card-inner">
+<div class="flip-card-front">
+<img src="avatar7.jpg" alt="Avatar" style="width:275px;height:275px;" alt="image">
+</div>
+<div class="flip-card-back">
+<h1>Dr. Shah</h1> 
+<p><b>Physiotherapist</b></p> 
+<p>Experience - 20 years<br>She got "Change Makers Award 2022" for best Physiotherapist .</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="column">
+<div class="flip-card">
+<div class="flip-card-inner">
+<div class="flip-card-front">
+<img src="avatar8.png" alt="Avatar" style="width:275px;height:275px;" alt="image">
+</div>
+<div class="flip-card-back">
+<h1>Dr. Sen</h1> 
+<p><b>Neurologist</b></p> 
+<p>Experience - 15 years<br>She has treated more than 200 patient.</p>
+</div>
+</div>
+</div>
+</div>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<center><a href="#top"><button class="button1">Click here to go to top</button></a></center>
+
+<script language="javascript">
+function bigImg(x) {
+  x.style.height = "210px";
+  x.style.width = "330px";
+}
+
+function normalImg(x) {
+  x.style.height = "175px";
+  x.style.width = "275px";
+}
+</script>
+
+</body>
+</html>
